fd82002f4efc4aa50a828cb4dde13a98a0fae00c
Masayidalfa <masayidalfarizqi@gmail.com>
2025-01-02 21:26:33 +0700

    Add animate-loop-scroll

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "loop-scroll": "loop-scroll 10s linear infinite",
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: {transfrom: "translateX(0)"},
+          to: {transform: "translateX(-75%)"},
+        }
+      }
+    },
   },
   plugins: [],
 }
