71ddad776237c1e8dd5f868c94cc8c8affd91473
Masayidalfa <masayidalfarizqi@gmail.com>
2025-01-06 21:12:10 +0700

    Memperbaiki animasi scroll tanpa batas otomatis

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,6 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      animation: {
-        "loop-scroll": "loop-scroll 10s linear infinite",
-      },
-      keyframes: {
-        "loop-scroll": {
-          from: {transfrom: "translateX(0)"},
-          to: {transform: "translateX(-75%)"},
-        }
-      },
       colors: {
         'primary': '#192a56',
         'secondary': '#0b8ea7',
